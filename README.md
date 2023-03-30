010c23a2893ee6e1f61d72f974b68c561c8f8b5f
Samuel Klutse <klutse.samuel@gmail.com>
2023-03-30 12:47:30 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# raspberry_usb_sanitizer
+# USB Cleaner
+
+This project is a simple script to clean up USB drives on Linux. It is intended to be run every time a USB drive is plugged in.
+The current implementation works works with a Raspberry Pi 4 running Raspbian.
+
+## Installation
+```bash
+git clone github.com/sparksam/pi_cleaner
+cd pi_cleaner
+python3.10 -m venv venv --upgrade-deps
+source venv/bin/activate
+pip install -r requirements.txt
+python main.py --help
+```
+
+## Author
+( Samuel Klutse ) [ https://samuelklutse.com ] ☕️
---
+++ src/main.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###########################################################################
+#                                                                         #
+# Copyright (C) 2014 Airbus DS CyberSecurity SAS. All rights reserved.    #
+# This document is the property of Cassidian CyberSecurity SAS, it may    #
+# not be circulated without prior licence                                 #
+#                                                                         #
+###########################################################################
+
+import argparse
+import json
+import os
+import sys
+from pathlib import Path
+
+import requests
+import urllib3
+
+urllib3.disable_warnings()
+
+
+class Colors(object):
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    BLUE = '\033[94m'
+    ORANGE = '\033[93m'
+    NO = '\033[0m'
+
+    def disable(self):
+        self.RED = ''
+        self.GREEN = ''
+        self.BLUE = ''
+        self.ORANGE = ''
+        self.NO = ''
+
+
+def showexec(code, status, description, exception=None):
+    """
+    Report a message with a pretty status display (Failed/Success) and status code
+    """
+    if len(description) > 65:
+        description = description[0:65] + "..."
+
+    if not status:
+        status = 'Success'
+
+    # Display the command
+
+    statuscolor = Colors.BLUE
+    if 200 <= code < 300:
+        statuscolor = Colors.GREEN
+
+    if 400 <= code or exception:
+        statuscolor = Colors.RED
+        status = 'Failed'
+
+    sys.stdout.write("Status : (%s)" % (statuscolor + status + Colors.NO) + " Code : (%s) " %
+                     (statuscolor + str(code) + Colors.NO) + "MSG : (%s)" % description + "\n")
+
+    sys.stdout.flush()
+
+
+def send_file(path, portal_url, visibility, session, key, tls_certificate):
+    """
+    La valeur 'user' indique le login
+    La valeur 'force' indique si on veut forcer une reanalyse
+    """
+
+    with open(path, 'rb') as f:
+        data = f.read()
+
+    url = portal_url + '/orion/api/v3.0/tasks'
+
+    json_data = {
+        'filename': os.path.basename(path),
+        'client_version': 1,
+        'visibility': visibility,
+        'force': False,
+        'callback_url': '',
+    }
+
+    form = {
+        'json': json.dumps(json_data)
+    }
+
+    files = {
+        'data': (os.path.basename(path), data)
+    }
+
+    headers = {
+        'apikey': key
+    }
+
+    try:
+        r = session.post(url, data=form, files=files,
+                         headers=headers, verify=tls_certificate)
+    except requests.exceptions.ConnectionError as e:
+        sys.stderr.write("Invalid portal address. Please try again.\n")
+        sys.stderr.write(f"{e}")
+        return
+
+    if r.status_code == 405 or r.status_code == 502:
+        showexec(r.status_code, "Failed", "Host may be not reachable ?")
+    else:
+        try:
+            if 'msg' in r.json():
+                showexec(r.status_code, r.json().get(
+                    'status'), r.json()['msg'])
+            else:
+                showexec(r.status_code, r.json().get('status'), r.json())
+        except Exception as ex:
+            showexec(r.status_code, None, r.text, exception=ex)
+
+
+def orion_scan(path: str, portal_url: str, visibility: str, key: str, tls_certificate: str):
+    """
+    Scan files with Orion
+    """
+
+    session = requests.Session()
+    print(f"Scanning {path} files with Orion at {portal_url}")
+    if os.path.isdir(path):
+        for root, dirs, files in os.walk(path):
+            for i in files:
+                n = os.path.join(root, i)
+                try:
+                    send_file(n, portal_url, visibility,
+                              session, key, tls_certificate)
+                except Exception as e:
+                    print("%s : %s" % (n, str(e)))
+    else:
+        send_file(path, portal_url, visibility, session, key, tls_certificate)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="USB Cleaner - A tool to clean USB devices from malware \nThe current version support the VirusTotal API, and Orion to scan the files. \nFollow me on Github: @sparksam")
+
+    parser.add_argument(
+        '--provider', help="Specify the Malware scanner to use: orion or virus_total", type=str, default='orion', choices=['orion', 'virus_total'])
+    parser.add_argument(
+        '--key', help="API key to use for provider", type=str, required=False)
+    parser.add_argument(
+        '--scan', help="Scan the files for malware", action='store_true')
+
+    parser.add_argument(
+        '--path', help="Path of the directory/file to scan", type=str)
+    parser.add_argument(
+        '--tls-certificate', help="Path to the TLS CA certificate bunde. eg: tls-certificate.pem", type=str, required=False)
+    parser.add_argument('--visibility', help='Visibility of generated task(s)',
+                        choices=['public', 'group', 'private'])
+    parser.add_argument(
+        '--format', help="Format the device", action='store_true')
+    parser.add_argument(
+        '--delete-malwares', help="Delete malicious files", action='store_true')
+    args = parser.parse_args()
+    env = Path(__file__).parent.parent / '.env'
+    if env.exists():
+        print(f"Environment file found at: {env}")
+        for line in env.read_text().split('\n'):
+            if len(line):
+                k, v = [s.strip() for s in line.split('=')]
+                os.environ[k] = v
+    else:
+        print(f"Environment file not found at: {env}")
+    if args.provider == 'orion':
+        portal_url = 'https://orion.cyberrange.cloud'
+        orion_key = args.key if args.key else os.environ.get('ORION_KEY')
+        if not orion_key:
+            print("Orion API key is required")
+            exit(1)
+        orion_scan(args.path, portal_url,  args.visibility,
+                   args.key, args.tls_certificate)
